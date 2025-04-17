6cc07d939adedab22b1616e5caa7fd8dfd4f7201
gagajooyo <gagajooyo@gmail.com>
2025-04-17 16:53:36 +0900

    연습8 조건문

---
+++ src/study/practice/practice08.java
@@ -0,0 +1,160 @@
+package study.practice;
+
+import java.util.Scanner;
+
+public class practice08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//1번
+		/*
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("1~24중 하나의 정수 입력 : ");
+		int hour = scanner.nextInt();
+		
+		
+		if (hour >= 1 && hour <= 6) {
+			System.out.println("오전입니다.");
+			System.out.println("이른 오전입니다.");
+		}else if (hour >= 7 && hour <= 12) {
+			System.out.println("오전입니다.");
+			System.out.println("늦은 오전입니다.");
+		}else if (hour > 12 && hour <= 18) {
+			System.out.println("오후입니다.");
+			System.out.println("이른 오후입니다.");
+		}else if (hour > 18 && hour <= 24) {
+			System.out.println("오후입니다.");
+			System.out.println("늦은 오후입니다.");
+		}else  {
+			System.out.println("잘못입력하셨습니다.");
+			
+		}
+		
+		
+		if (hour <= 6 && hour <= 12) {
+			System.out.println("오전입니다");
+			
+			if(hour <= 6) {
+				System.out.println("이른 오전입니다.");
+			}else {
+				System.out.println("늦은 오전입니다.");
+			}
+				
+		}else if (hour > 12 && hour <= 24) {
+			if(hour <= 18) {
+				System.out.println("이른 오후입니다.");
+			}else {
+				System.out.println("늦은 오후입니다.");
+			}
+		}else {
+			System.out.println("잘못입력하셨습니다.");
+		}
+		
+		
+		if(hour >=1 && hour <=24) {
+			if(hour <= 12) {
+				if(hour <=6) {
+				
+				}else {
+					
+				}
+			}
+		}else {
+			if(hour <= 18) {
+				
+			}	else {
+				
+			}
+		}
+		else {
+			System.out.println("잘못입력했습니다.");
+		}
+		*/
+		
+		
+		
+		/*
+		//2번 아래 코드를 if문으로 변경
+		
+		
+		if (diceNum == 1) {
+			System.out.println("1");
+		}else if (diceNum == 2) {
+			System.out.println("2");
+		}else if (diceNum == 3) {
+			System.out.println("3");
+			System.out.println("4");
+			System.out.println("5");
+		}else if (diceNum == 4) {
+			System.out.println("4");
+			System.out.println("5");
+		}else if (diceNum == 5) {
+			System.out.println("5");
+		}else if (diceNum == 6) {
+			System.out.println("6");
+		}
+	}
+	*/
+	/*
+	//3번 아래 코드를 swtich 문으로 변경
+	
+		//String 값 비교
+	// localNum == "031" 비교 가 안된다.
+	// localNum.equals("031") 이렇게 작성해야 비교가 된다.
+	// true/false 결과 도출 boolean 타입의 결과와 동일
+	
+	String localNum = "0";
+	
+	switch (localNum) {
+	case "031" : System.out.println("경기도입니다");
+	break;
+	case "02" : System.out.println("서울입니다.");
+	break;
+	case "041" : System.out.println("충남입니다");
+	break;
+	case "051" : System.out.println("부산입니다");
+	break;
+	//	default:
+		
+	}
+		
+		*/
+		
+		//4번 아래 코드를 switch 문으로 변경
+		
+		int score =95 ;
+		
+		if( score >= 90) {
+					System.out.println("학점 A");
+					} else if ( score >= 80) {
+					System.out.println("학점 B");
+					} else if (score >= 70) {
+					System.out.println("학점 C");
+					} else if (score < 70) {
+					System.out.println("학점 F");
+					}
+		
+		
+		int num 10 = sxore/10
+		
+		switch (score / 10)
+		case 10;
+		case 9;
+		System.out.println("학점 A");
+		case 8;
+		System.out.println("학점 B");
+		case 7;
+		System.out.println("학점 C");
+		case 
+		System.out.println("학점 F");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
