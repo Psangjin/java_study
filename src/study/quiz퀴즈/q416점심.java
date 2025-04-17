95cff747f627b55d34663b61355ec6af0aa30f2d
gagajooyo <gagajooyo@gmail.com>
2025-04-17 10:49:46 +0900

    4.16quiz

---
+++ src/study/quiz퀴즈/q416점심.java
@@ -0,0 +1,87 @@
+package study.quiz퀴즈;
+
+import java.util.Scanner;
+
+public class q416점심 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 1번. 삼각형의 밑변과 높이를 입력받아서 넓이를 출력하세요. (소수점까지 표현)
+		// 삼각형의 넓이 = 밑변 * 높이 / 2 ;
+	/*	
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("삼각형의 밑변 : ");
+		int num1 = scanner.nextInt();
+		System.out.println("밑변=" + num1);
+		
+		System.out.print("삼각형의 높이 : " );
+		int num2 = scanner.nextInt();
+		System.out.println("밑변=" + num2);
+		
+		
+		System.out.println(num1*num2 /(double) 2);
+		*/
+
+		// 2번. 사다리꼴의 윗변과 밑변과 높이를 입력받아서 넓이를 출력하세요. (소수점까지 표현)
+		// 사다리꼴의 넓이 = (윗변 길이 + 밑변 길이) * 높이 / 2;
+/*
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("사다리꼴의 윗변 : ");
+		int num1 = scanner.nextInt();
+		System.out.println("윗변=" + num1);
+		
+		System.out.print("사다리꼴의 밑변 : " );
+		int num2 = scanner.nextInt();
+		System.out.println("밑변=" + num2);
+		
+		System.out.print("사다리꼴의 높이 : " );
+		int num3 = scanner.nextInt();
+		System.out.println("높이=" + num3);
+		
+		System.out.println((num1+num2) * num3 /(double)2);
+		
+*/		
+		// 3번. 우리반은 24명입니다. 과자가 100개 있습니다.
+		// 모두에게 공정하게 동일한 숫자의 과자를 나누어주면
+		// 한 명당 몇 개의 과자를 받게되는지와 나누어주고 나서 몇개의 과자가 남는지 출력하세요.
+	/*	
+		
+		int x= 100 % 24;
+		System.out.println(x);
+		
+		*/
+
+		// 4번. 세자리의 정수를 입력 받아서 백의자리, 십의자리, 일의자리를 각각 출력하세요.
+		// 158
+		// 백의자리 : 1
+		// 십의자리 : 5
+		// 일의자리 : 8
+		
+
+		
+		
+		
+		
+		
+		// 5번 x와 y 변수 안에 들어있는 서로의 값을 바꾸기
+		int x = 10;
+		int y = 20;
+		System.out.println("x="+x+ " y="+y);
+		//x와 y를 바꾼 작업
+		//
+		
+		int z = x;
+		
+		x = y;  y= z; 
+		
+				
+		//
+		System.out.println("x="+x+ " y="+y);
+		
+		
+	}
+
+}
