a4f185a40fb025a4059814a8cde172716c70fe34
gagajooyo <gagajooyo@gmail.com>
2025-05-01 14:21:09 +0900

    싱글톤 패턴

---
+++ src/study/cls/cls04/Singleton.java
@@ -0,0 +1,17 @@
+package study.cls.cls04;
+
+public class Singleton {
+	//싱글톤 : 전체~ 에서 1개만 존재
+	
+	private static Singleton singleton = new Singleton();
+	
+	//외부에서 생성 방지
+	private Singleton( ) {		
+	}
+	
+	public static Singleton gatInstance() {
+		return singleton;
+	}
+	
+	
+}
---
+++ src/study/cls/cls04/SingletonMain.java
@@ -0,0 +1,24 @@
+package study.cls.cls04;
+
+public class SingletonMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		Singleton st1 = new Singleton();
+//		Singleton st2 = new Singleton();
+//		Singleton st3 = new Singleton();
+//		Singleton st4 = new Singleton();
+		
+		Singleton st = null;
+		
+//		Singleton ss = new Singleton();
+//		ss.singleton
+		st = Singleton.getInstance();
+		
+		Singleton st2 = Singleton.getInstance();
+		Singleton st3 = Singleton.getInstance();
+		
+	}
+
+}
