07054fb98eca7a07e9fdb4b46018b7e34b786af2
gagajooyo <gagajooyo@gmail.com>
2025-05-09 14:07:47 +0900

    연습 38 상속

---
+++ src/study/quiz퀴즈/q508낮1/Wizard.java
@@ -0,0 +1,12 @@
+package study.quiz퀴즈.q508낮1;
+
+public class Wizard extends Novice {
+	
+	
+	int mp;
+	
+	
+	public void fireball() {
+		System.out.printf("[%s]의 파이어볼@@\n", name);
+	}
+}
---
+++ src/study/quiz퀴즈/q508낮2/Animal.java
@@ -0,0 +1,11 @@
+package study.quiz퀴즈.q508낮2;
+
+public class  Animal {
+	String name; // 이름
+
+	public void cry() {
+	System.out.println("동물이 웁니다!");
+	}
+	}
+
+
