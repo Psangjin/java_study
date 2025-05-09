a76eb9298498cd9265fcbef46888cf90be47635f
gagajooyo <gagajooyo@gmail.com>
2025-05-09 14:06:34 +0900

    매개변수, 리턴타입 다형성

---
+++ src/study/inherit/inherit06/Driver.java
@@ -0,0 +1,10 @@
+package study.inherit.inherit06;
+
+public class Driver {
+	public void drive(Bus bus) {
+		bus.run() ;
+	}
+	public void drive(Taxi taxi) {
+		taxi.run();
+	}
+}
---
+++ src/study/inherit/inherit06/Main.java
@@ -0,0 +1,16 @@
+package study.inherit.inherit06;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Driver d = new Driver();
+		Taxi t = new Taxi();
+		Bus b = new Bus();
+		
+		d.drive(b);
+	}
+
+}
