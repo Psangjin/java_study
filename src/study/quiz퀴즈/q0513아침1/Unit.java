66df377840a5389497155a6da65e30c853394f3f
gagajooyo <gagajooyo@gmail.com>
2025-05-13 10:19:04 +0900

    연습44 클래스 구조

---
+++ src/study/quiz퀴즈/q0513아침1/Unit.java
@@ -0,0 +1,19 @@
+package study.quiz퀴즈.q0513아침1;
+
+public class Unit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+//	아래 세 개의 클래스로부터 공통부분을 뽑아서 Unit 이라는 클래스를 만들고 이 ,
+//	클래스를 상속받도록 코드를 변경하시오.
+//	*조건
+//	1) 멈추는 동작은 모두 동일하게 제자리에 정지한다.
+//	2) 움직이는 동작은 걷는거, 날기, 바퀴구르기 다르기 때문에 각각 다르게 정의한다.
+		
+		
+		
+	}
+
+}
